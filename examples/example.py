907bca32afa6487d2aa282423b81de2e969606ab
WallD3v <109732229+WallD3v@users.noreply.github.com>
2024-04-08 19:54:59 +0300

    Add files via upload

---
+++ examples/example.py
@@ -0,0 +1,13 @@
+from FinPay.api import FinPayAPI, FinPayInvoice
+
+api = FinPayAPI(1337,
+                '*****',
+                '*****',)
+
+invoice: FinPayInvoice = api.create_invoice("Invoice-123", "Just description.", 999, "card_cis", "RU", "RUB")
+
+print(invoice.check_status())
+print(invoice)
+print(invoice.url)
+
+invoice.cancel_invoice()
